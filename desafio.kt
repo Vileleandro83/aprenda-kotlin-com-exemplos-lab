86cdad09c48b56cb0e96f5521b3214f5fcad4794
vile_leandro <135434246+Vileleandro83@users.noreply.github.com>
2024-02-03 10:59:27 -0300

    codigo de matricula

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Aluno(val nome: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
+
+
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    private val alunosMatriculados = mutableListOf<Aluno>()
+
+    
+    fun matricular(vararg alunos: Aluno) {
+        alunosMatriculados.addAll(alunos)
+        println("Aluno(s) matriculados ${alunos.joinToString(", ") { it.nome }} ")
+    }
+
+    
+    fun exibirInformacoes() {
+        println("Formação: $nome")
+        println("Nível: $nivel")
+        println("Conteúdos:")
+        conteudos.forEach { println("- ${it.nome} (${it.duracao} minutos)") }
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val aluno1 = Aluno("Leandro")
+    val aluno2 = Aluno("Isaac")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+
+    val formacao = Formacao("Desenvolvimento em Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+
+    formacao.matricular(aluno1, aluno2)
+
+    formacao.exibirInformacoes()
 }
